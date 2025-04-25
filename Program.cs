3a29b9fbec48666bd8fab5b6beea82ab26fe366a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 20:46:22 -0300

    Eu amo números negativos

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double numero;
+
+Console.Clear();
+
+Console.Write("Digite um número: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+if (numero > 0)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("\nNúmero correto!");
+}
+else
+{
+ Console.ForegroundColor = ConsoleColor.Red;
+ Console.WriteLine("\nVocê digitou um número negativo");
+}
+
+Console.ResetColor();
